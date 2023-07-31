c1ac271babd446aa9353a12adb4da28d6662a7e4
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2023-07-31 21:45:08 +0630

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -1,3 +1,3 @@
 echo "ttyd serving at port 6745 with username:pass as kali:kali"
 chmod +x /usr/local/bin/ttyd_linux
-/usr/local/bin/ttyd_linux -p 443 -c kali:kali bash
+/usr/local/bin/ttyd_linux -p 80 -c kali:kali bash
