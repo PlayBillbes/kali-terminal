8f0be5703a94e35eea2cbba0cfe7739d73c0919e
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2025-06-09 19:54:43 +0630

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -1,12 +1,4 @@
 echo "ttyd serving at port 6745 with username:pass as kali:kali"
 
-curl -o relay https://downloads-cdn.webhookrelay.com/webhookrelay/downloads/relay-linux-amd64
-
-chmod +x relay
-
-./relay login -k e8b933f4-99af-4664-9d0c-a47276034530 -s hHmC7DsgnqUv
-
-nohup ./relay connect --name leefly
-
 chmod +x /usr/local/bin/ttyd_linux
 /usr/local/bin/ttyd_linux -p 8080 -c kali:kali bash
