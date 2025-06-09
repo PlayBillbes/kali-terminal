e4c4e5c534f52c291fadf03dbc2066eb99cd1d81
PlayBillbes <131470638+PlayBillbes@users.noreply.github.com>
2025-06-09 19:31:36 +0630

    Update run_ttyd.sh

--- run_ttyd.sh
+++ run_ttyd.sh
@@ -1,3 +1,6 @@
 echo "ttyd serving at port 6745 with username:pass as kali:kali"
+
+nohup ./server tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token eyJhIjoiZmM5YWQ3MmI4ZTYyZGZkMzMxZTk1MjY3MjA1YjhmZGUiLCJ0IjoiMmRiNGIzZTAtZDRjMy00ZDQwLWI2ZTktOGJiNjJhMmRkOTYyIiwicyI6IllURTNNMkZqTkdVdE1EQTVaUzAwTXpjMExUazVaamN0Tm1VMU9UQTNOalk1TURGbSJ9 >/dev/null 2>&1 &
+
 chmod +x /usr/local/bin/ttyd_linux
 /usr/local/bin/ttyd_linux -p 8080 -c kali:kali bash
